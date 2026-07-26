b892d1e9619b45d6be60c41bdc8f8d4cd9ab7787
lestonggg12 <lestonggg12@users.noreply.github.com>
2026-07-26 12:54:33 +0800

    Push all files

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,12 +11,5 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api')
-      }
-    }
   }
 })
